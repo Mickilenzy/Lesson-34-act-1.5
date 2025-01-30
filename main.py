52852cb1b903df72da8f91a1e43185b12708124c
Mickilenzy <ilerioluwamichael297@gmail.com>
2025-01-30 17:53:13 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+print("hello")
+print("Michael")
+print(67)
+print("hi" , 57, 58.56)
+
+
+import keyword
+print(keyword.kwlist)
+
+print(5 + 3)
+print(45 - 6)
+print(55 * 7)
+print(23 / 6)
+
+name = input("What is ypur name ?")
+print("Hello, " + name + "!")
+
+age = int(input("What is your age?"))
+print("You are " + str(age) + "years old.")
+
+subject = input("What is your favourite subject?")
+print("Your favourite subject is " + subject  + ".")
